2ca078a6d0dbbdeb1cb1362f9d69c64fcdd6b501
dariomnalerio <dariomnalerio@gmail.com>
2023-04-02 17:04:30 -0300

    Add task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+Exports data in the CSV format.
+"""
+import requests
+from sys import argv
+import csv
+
+
+try:
+    user_id = int(argv[1])
+except IndexError:
+    print("You must enter an integer")
+    exit()
+
+response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(user_id))
+user_info = response.json()
+
+response1 = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(user_id))
+user_tasks = response1.json()
+
+completed_tasks = 0
+total_tasks = 0
+for task in user_tasks:
+    if task['completed'] is True:
+        completed_tasks += 1
+    total_tasks += 1
+
+print("Employee {} is done with tasks({}/{}):"
+      .format(user_info['name'], completed_tasks, total_tasks))
+
+task_title = [todos['title'] for todos in user_tasks
+              if todos['completed'] is True]
+for title in task_title:
+    print("\t {}".format(title))
+
+file = '{}.csv'.format(user_id)
+
+with open(file, 'w') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+    for task in user_tasks:
+        writer.writerow([user_id, user_info['username'],
+                        task['completed'], task['title']])
+ 
